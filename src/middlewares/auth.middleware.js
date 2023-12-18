10e59f554361d05396ab6404526bb86c92f31aea
Aashish70 <ashishsingh70002@gmail.com>
2023-12-18 13:11:33 +0530

    fix the issue

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
-import {User} from "../models/user.model"
+import {User} from "../models/user.model.js"
 
 
 export const verifyJWT = asyncHandler(async(req, res, next) => {
@@ -11,7 +11,7 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
         if(!token){
             throw new ApiError(401, "Unathourized request")
         }
-        const decodedToken = jwt.verify(token, proccess.env.REFRESH_TOKEN_SECRET)
+        const decodedToken = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
     
         const user = await User.findById(decodedToken?._id)
         .select("-password, -refreshToken")
